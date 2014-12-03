f3dab176bd6281ee262d88fd7af858a6366e6158
Stefan Berger <s.berger81@gmail.com>
2014-12-03 17:53:28 +0100

    Update media_codecs.xml, add libstagefright codec includes

--- golden.mk
+++ golden.mk
@@ -69,6 +69,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/etc/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/configs/omxloaders:system/omxloaders
 PRODUCT_PACKAGES += \
     libomxil-bellagio
