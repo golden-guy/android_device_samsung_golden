d4eaa2a2e6aab18f93b4ed32c99ca98f99e15f16
Stefan Berger <s.berger81@gmail.com>
2014-12-26 11:40:09 +0100

    golden: Don't build wifi HAL -see https://gerrit.omnirom.org/#/c/11024

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,7 @@ WIFI_DRIVER_MODULE_NAME := "dhd"
 WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 WIFI_DRIVER_MODULE_AP_ARG := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 BOARD_NO_APSME_ATTR := true
+BOARD_NO_WIFI_HAL := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
