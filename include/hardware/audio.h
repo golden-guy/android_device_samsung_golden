f16b3a6f88483df65cd7c0e5a973904265ade0ce
Stefan Berger <s.berger81@gmail.com>
2014-12-30 21:54:32 +0100

    Fix in-call audio

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -734,7 +734,7 @@ struct audio_hw_device {
     void (*close_output_stream)(struct audio_hw_device *dev,
                                 struct audio_stream_out* stream_out);
 
-#if defined (QCOM_HARDWARE) || defined (STE_SAMSUNG_HARDWARE)
+#if defined (QCOM_HARDWARE)
     /** This method creates and opens the audio hardware output
      *  for broadcast stream */
     int (*open_broadcast_stream)(struct audio_hw_device *dev, uint32_t devices,
